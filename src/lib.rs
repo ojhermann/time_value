ebac735614bd239f114273c91ebaf6ad4e0c2a34
Otto Hermann <ojhermann@gmail.com>
2021-01-28 15:12:01 +0000

    Autoformat lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! A collection of functions related to time value analysis.
 
 pub mod future_value;
+
 pub mod irr {
     //! Functions and structs for calculating the internal rate of return (IRR) of a series of cash flows.
     pub mod bisection {
@@ -9,4 +10,5 @@ pub mod irr {
         pub mod function;
     }
 }
+
 pub mod present_value;
