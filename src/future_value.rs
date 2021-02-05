71f5dc08fd4025c90c6916290847ca220cae54cd
Otto Hermann <ojhermann@gmail.com>
2021-02-05 00:16:50 +0000

    Correct bizarre error

--- src/future_value.rs
+++ src/future_value.rs
@@ -50,51 +50,51 @@ use std::slice::Iter;
 /// assert!(abs(value - expected_value) < 0.001)
 /// ```
 pub fn from_pv_and_expected_rates<T>(present_value: &T, expected_rates: Iter<T>) -> T
-where
-    T: Float + Product<T>,
+    where
+        T: Float + Product<T>,
 {
-    *present_value * expected_rates.map(|rate| T::one() + *rate).product::<T>()
+    expected_rates.fold(*present_value, |acc, x| acc * (T::one() + *x))
 }
 
 #[cfg(test)]
 mod from_pv_and_expected_rates_tests {
-    use crate::future_value;
+    use crate::future_value::from_pv_and_expected_rates as fv;
+    use num::abs;
+
 
     #[test]
-    fn it_works_with_rates_f32() {
-        let present_value: f32 = 10.0;
-        let rates: Vec<f32> = vec![1.0, 2.0, 3.0];
-        let expected_value: f32 = 240.0;
-        let value: f32 = future_value::from_pv_and_expected_rates(&present_value, rates.iter());
-        assert_eq!(value, expected_value);
-        assert_eq!(rates.len(), 3);
-    }
-
-    #[test]
-    fn it_works_without_rates_f32() {
-        let present_value: f32 = 10.0;
-        let rates: Vec<f32> = vec![];
-        let value: f32 = future_value::from_pv_and_expected_rates(&present_value, rates.iter());
-        assert_eq!(value, present_value);
-        assert_eq!(rates.len(), 0);
-    }
-
-    #[test]
-    fn it_works_with_rates_f64() {
+    fn it_works_with_no_rates() {
         let present_value: f64 = 10.0;
-        let rates: Vec<f64> = vec![1.0, 2.0, 3.0];
-        let expected_value: f64 = 240.0;
-        let value: f64 = future_value::from_pv_and_expected_rates(&present_value, rates.iter());
-        assert_eq!(value, expected_value);
-        assert_eq!(rates.len(), 3);
+        let expected_rates: Vec<f64> = vec![];
+        let future_value: f64 = fv(&present_value, expected_rates.iter());
+        assert_eq!(future_value, present_value);
+        assert_eq!(expected_rates.len(), 0);
     }
 
     #[test]
-    fn it_works_without_rates_f64() {
+    fn it_works_with_positive_rates() {
+        let present_value: f32 = 10.0;
+        let rates: Vec<f32> = vec![0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
+        let expected_value: f32 = 19.48;
+        let value: f32 = fv(&present_value, rates.iter());
+        assert!(abs(value - expected_value) < 0.01);
+    }
+
+    #[test]
+    fn it_works_with_negative_rates() {
+        let present_value: f32 = 10.0;
+        let rates: Vec<f32> = vec![-0.02, -0.02, -0.02, -0.02, -20.0, -0.02, -0.02, -0.02];
+        let expected_value: f32 = -164.94;
+        let value: f32 = fv(&present_value, rates.iter());
+        assert!(abs(value - expected_value) < 0.01);
+    }
+
+    #[test]
+    fn it_works_with_mixed_rates() {
         let present_value: f64 = 10.0;
-        let rates: Vec<f64> = vec![];
-        let value: f64 = future_value::from_pv_and_expected_rates(&present_value, rates.iter());
-        assert_eq!(value, present_value);
-        assert_eq!(rates.len(), 0);
+        let rates: Vec<f64> = vec![0.02, 0.04, -0.20, 0.00, -0.08, 0.20, 0.03, -0.02];
+        let expected_value: f64 = 9.46;
+        let value: f64 = fv(&present_value, rates.iter());
+        assert!(abs(value - expected_value) < 0.01);
     }
 }
